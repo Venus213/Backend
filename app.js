b77a087502bc42ea18b169c322b12d7d7b02cc22
amisavani39 <157886263+amisavani39@users.noreply.github.com>
2024-03-28 20:35:46 +0530

    timeout update

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 1000);
   res.render('error');
 });
 
